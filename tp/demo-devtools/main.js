de5eace3ea8694ed8e50b2e5ab32287989c1bb52
FazioNico <contact@nicolasfazio.ch>
2016-09-19 11:59:55 +0200

    upd exo

--- tp/demo-devtools/main.js
+++ tp/demo-devtools/main.js
@@ -1,21 +1,27 @@
 // Exo 1
 
-let a = 10;
+let a = 2011;
 let b = '5';
 let debuggerTest1 = document.getElementsByClassName('debuggerTest1')[0];
-debuggerTest1.innerHTML = `<h1>${a+b}</h1>`;
+debuggerTest1.innerHTML = '<h1>'+a+b+'</h1>';
+console.log( 'Exo1: résoudre bug de calcule');
 
 // Exo 2
+let debuggerTest2 = document.getElementsByClassName('debuggerTest2')[0];
+let variable = 'Hello world!'
+debuggerTest2.innerHTML = '<h1>'+'variable'+'<br> Welcome in Chrome DevTools!'+'</h1>';
+console.log( `Exo2: résoudre bug d'affichage`);
+// Exo 3
 class DebuggerTest{
   constructor(){
-    this.title = 'Hello and welcome in Chrome DevTools!';
+    this.title = 'Yeah! Javascript is cool!';
   }
   displayTitle(){
 
     setTimeout(function(){
-      let debuggerTest2 = document.getElementsByClassName('debuggerTest2')[0];
-      debuggerTest2.innerHTML = `<h1>${this.title}</h1>`
-      console.log( 'this.title ->', this.title);
+      let debuggerTest3 = document.getElementsByClassName('debuggerTest3')[0];
+      debuggerTest3.innerHTML = `<h1>${this.title}</h1>`
+      console.log( 'Exo3: bug avec this.title ->', this.title);
     }, 100);
   }
 }
