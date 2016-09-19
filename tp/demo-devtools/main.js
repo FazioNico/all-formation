fe8d561f8833f5098986caf5807653d5943ad984
FazioNico <contact@nicolasfazio.ch>
2016-09-19 11:39:01 +0200

    upd exo

--- tp/demo-devtools/main.js
+++ tp/demo-devtools/main.js
@@ -1,3 +1,11 @@
+// Exo 1
+
+let a = 10;
+let b = '5';
+let debuggerTest1 = document.getElementsByClassName('debuggerTest1')[0];
+debuggerTest1.innerHTML = `<h1>${a+b}</h1>`;
+
+// Exo 2
 class DebuggerTest{
   constructor(){
     this.title = 'Hello and welcome in Chrome DevTools!';
@@ -5,7 +13,8 @@ class DebuggerTest{
   displayTitle(){
 
     setTimeout(function(){
-      document.getElementsByClassName('debuggerTest')[0].innerHTML = `<h1>${this.title}</h1>`
+      let debuggerTest2 = document.getElementsByClassName('debuggerTest2')[0];
+      debuggerTest2.innerHTML = `<h1>${this.title}</h1>`
       console.log( 'this.title ->', this.title);
     }, 100);
   }
